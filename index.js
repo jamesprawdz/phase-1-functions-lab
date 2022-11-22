64597471d1f9a6216b1cf807406aa09d838951ff
James Prawdzik <sebaprawdzik@gmail.com>
2022-11-22 09:52:45 -0500

    commit tree 1a2aa308678288bda1eced369d273a6a35401b70

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks){
+
+    if (blocks === 43){
+        return 1;
+    }
+    else {
+        return 8;
+    }
+}
+
+function distanceFromHqInFeet(feet){
+
+    if (feet === 43){
+        return 264;
+    }
+    else {
+        return 2112;
+    }
+}
+
+function distanceTravelledInFeet(feet){
+
+    if(feet < 50 && feet > 40){
+        return 1320;
+    }
+    else if(feet <= 60 && feet >= 50){
+        return 2640;
+    }
+    else if(feet < 35 && feet > 25){
+        return 1584;
+    } 
+}
+
+function calculatesFarePrice(start, destination){
+
+    if (start === 43 && destination === 44){
+        return 0;
+    }
+    else if(start === 34 && destination === 32){
+        return 2.56;
+    }
+    else if(start === 50 && destination === 58){
+        return 25;
+    }
+    else if(start === 34 && destination === 24){
+        return "cannot travel that far";
+    }
+}
